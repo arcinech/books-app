a53a26a45494c540686a0378dc912e4b8b2c0919
Marcin Lech <arcinech@gmail.com>
2022-03-10 14:21:42 +0100

    Add: app.initActions

--- src/js/script.js
+++ src/js/script.js
@@ -7,16 +7,24 @@
     containerOf: {
       bookList: '.books-list',
       book: '.book',
+      filters: '.filters',
     },
     innerBook: {
       header: '.book__header',
       basePrising: '.product__base-price',
-      bookImage: '.book_image',
+      bookImage: '.book__image',
       bookRating: '.book__rating',
     },
   };
 
-  const favoriteBooks = [];
+  const names = {
+    bookImage: 'book__image',
+    favorite: 'favorite',
+    dataId: 'data-id',
+  };
+
+  const favoriteBooks = []; // eslint-disable-line no-unused-vars
+  const filters = []; // eslint-disable-line no-unused-vars
 
   const templates = {
     bookList: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML)
@@ -28,8 +36,6 @@
     constructor(data){
       const thisBook = this;
       thisBook.data = data;
-
-      console.log(thisBook.data);
 
       thisBook.renderBooks();
     }
@@ -51,7 +57,6 @@
       const thisApp = this;
 
       for(let bookData of thisApp.data.books){
-        console.log(thisApp.data.books);
         new Book(bookData);
       }
     },
@@ -60,7 +65,40 @@
       const thisApp = this;
   
       thisApp.data = dataSource;
-      console.log(thisApp.data);
+    },
+
+    initActions: function(){
+      document.querySelector(select.containerOf.bookList).addEventListener('dblclick', function(event){
+        const clickedImage = event.target.offsetParent;
+
+        if(clickedImage.classList.contains(names.bookImage)){
+          const id = clickedImage.getAttribute(names.dataId);
+          if(clickedImage.classList.contains(names.favorite)){
+            clickedImage.classList.remove(names.favorite);
+            const index = favoriteBooks.indexOf(id);
+            favoriteBooks.splice(index, 1);
+          } else {
+            clickedImage.classList.add(names.favorite);
+            if (!favoriteBooks.includes({id: id})) favoriteBooks.push({id: id});
+          }
+          console.log(favoriteBooks);
+        }
+      });
+
+      document.querySelector(select.containerOf.filters).addEventListener('click', function(event){
+        if(event.target.tagName == 'INPUT' && event.target.type == 'checkbox' && event.target.name == 'filter'){
+          console.log(event.target.value);
+          const filterValue = event.target.value;
+
+          if(event.target.checked) filters.push(filterValue);
+          else {
+            const index = filters.indexOf(filterValue);
+            filters.splice(index, 1);
+          }
+
+          console.log(filters);
+        }
+      });
     },
 
     init: function(){
@@ -68,6 +106,7 @@
         
       thisApp.initData();
       thisApp.initBooks();
+      thisApp.initActions();
     }
   
   };
